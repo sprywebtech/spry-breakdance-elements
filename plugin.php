956df73d3306c83beb4a294118be08413ce9fc77
sprywebtech <dougmorris06@gmail.com>
2026-07-15 06:45:55 -0500

    Add plugin.php

---
+++ plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Spry Breakdance Elements
+ * Plugin URI: https://sprywebtech.com/
+ * Description: A reusable collection of custom Breakdance elements by Spry Web Tech.
+ * Author: Spry Web Tech
+ * Author URI: https://sprywebtech.com/
+ * License: GPLv2 or later
+ * Text Domain: spry-breakdance-elements
+ * Version: 1.0.0
+ */
+
+namespace SpryWebTechBreakdanceElements;
+
+use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('breakdance_loaded', function () {
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
+        'SpryWebTechBreakdanceElements',
+        'element',
+        'Spry Web Tech',
+        false
+    );
+}, 9);
